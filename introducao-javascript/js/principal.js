2810efe60e326c28fe3cff9fa321179593737793
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-07-29 23:35:33 -0300

    Projeto capitulo 1

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,9 @@
+    /*    3) Boas práticas
+    //
+    //    Boa prática - Não faça a busca por tag, utilize outros meios para não ficar atrelado ao html
+    //    um bom exemplo é utilizando classes, id ou seletor do CSS que são aceitas no querySelector.
+    //    titulo.textContent = ".titulo"; //retorna os elementos da classe titulo.
+
+          lembre-se, separar o que é html, css e js
+          cada um no seu quadrado.
+    */
