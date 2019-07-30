7d69bbcd80b4a11e81aeb11c6c73f9e79e552899
Valéria Regina <42319403+ValeriaRoyal@users.noreply.github.com>
2019-07-30 00:29:12 -0300

    Cap 2 - utilizando textContent
    Modificando valores da tabela no js

--- Capítulo 2/js/principal.js
+++ Capítulo 2/js/principal.js
@@ -1,9 +1,13 @@
-    /*    3) Boas práticas
-    //
-    //    Boa prática - Não faça a busca por tag, utilize outros meios para não ficar atrelado ao html
-    //    um bom exemplo é utilizando classes, id ou seletor do CSS que são aceitas no querySelector.
-    //    titulo.textContent = ".titulo"; //retorna os elementos da classe titulo.
+var paciente = document.querySelector("#first-paciente");
 
-          lembre-se, separar o que é html, css e js
-          cada um no seu quadrado.
-    */
+var tdpeso = paciente.querySelector(".info-peso");
+var Peso = tdpeso.textContent;
+var tdalt = paciente.querySelector(".info-altura");
+var Altura = tdalt.textContent;
+
+
+var IMC = Peso/(Altura*Altura);
+
+
+var imc = paciente.querySelector(".info-imc");
+imc.textContent = IMC;
